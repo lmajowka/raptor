646c74677b72a707895bc9e4c2c0b5035e7adee5
Leo <leo@crossrider.com>
2015-01-27 22:00:49 +0200

    rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Raptor::Application.config.secret_key_base = '47abb28670bc1c1ce9100ebfa65eb9abe39caf2071e94c237c53043df6b825b16f63c9ff3c03db86f33daa07e2a066dd87d68cef38de5da2c7e0cc70fbb0832c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Raptor::Application.config.session_store :cookie_store, key: '_raptor_session'
